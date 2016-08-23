e9cd84785c6722901c770485660a6f3f522f4d0a
Ethan-Shap <ethancodes@gmail.com>
2016-08-22 21:18:03 -0700

    Added Assets, Added Follow Script
    Added a follow object script and some effects from standard assets

---
+++ 3D Tower Defense/Assets/FollowObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public GameObject objToFollow;
+
+    public bool offset = true;
+    public bool rotateWithObj = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!objToFollow)
+            throw new System.NullReferenceException("No Object to Follow");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
