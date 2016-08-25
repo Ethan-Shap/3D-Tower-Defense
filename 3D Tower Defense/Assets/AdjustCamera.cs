7733622fc674255d711b891a8338a683c1e448bd
Ethan-Shap <ethancodes@gmail.com>
2016-08-25 15:19:01 -0700

    Fixed Bug where camera would not move
    Camera would not move because there was an error in the adjust camera
    code. no camera parent for the touch input to manipulate

--- 3D Tower Defense/Assets/AdjustCamera.cs
+++ 3D Tower Defense/Assets/AdjustCamera.cs
@@ -10,22 +10,17 @@ public class AdjustCamera : MonoBehaviour {
     public float expectedFPS = 30f;
 
     [SerializeField]
-    private Camera mainCamera;
-
-    // Vars for Rotating Camera
-    private Vector2 previousMousePos;
-    private Vector3 rotDir;
+    private Transform cameraParent;
 
     // Vars for zooming camera
     [SerializeField]
-    private float previousDist;
     private float stopZoomingDist = 15f;
 
     // Use this for initialization
     void Start ()
     {
-        if (!mainCamera)
-            throw new System.NullReferenceException("No main camera for camera adjuster");
+        if (!cameraParent)
+            throw new System.NullReferenceException("No main parent for camera adjuster to manipulate camera");
 	}
 	
 	// Update is called once per frame
@@ -33,50 +28,69 @@ public class AdjustCamera : MonoBehaviour {
     {
         if (Input.touchCount == 1)
         {
-            Rotate(Vector3.zero);
-            mainCamera.transform.rotation = Quaternion.Euler(new Vector3(mainCamera.transform.rotation.eulerAngles.x, mainCamera.transform.rotation.eulerAngles.y, 0f));
+            Rotate();
         } else if(Input.touchCount == 2)
         {
             Zoom();
-        } else
-        {
-            mainCamera.transform.rotation = Quaternion.Euler(new Vector3(mainCamera.transform.rotation.eulerAngles.x, mainCamera.transform.rotation.eulerAngles.y, 0f));
-        }
+        } 
 	}
 
     private void Zoom()
     {
-        // Calculate distance between touches 
+        //// Calculate distance between touches 
         float curDist = Vector3.SqrMagnitude(Input.touches[0].position - Input.touches[1].position);
+        float prevDist = Vector3.SqrMagnitude(Input.touches[0].deltaPosition - Input.touches[1].deltaPosition);
 
-        // Determine which way too zoom
-        Vector3 zoomDir = curDist > previousDist ? mainCamera.transform.forward : -mainCamera.transform.forward;
-        zoomDir = mainCamera.transform.InverseTransformDirection(zoomDir);
+        //// Determine which way too zoom
+        //Vector3 zoomDir = curDist > previousDist ? mainCamera.transform.forward : -mainCamera.transform.forward;
+        //zoomDir = mainCamera.transform.InverseTransformDirection(zoomDir);
 
-        float dst = Vector3.SqrMagnitude(mainCamera.transform.position - Vector3.zero);
+        //float dst = Vector3.SqrMagnitude(mainCamera.transform.position - Vector3.zero);
 
-        if (dst > stopZoomingDist || zoomDir == -mainCamera.transform.forward)
-        {
-            // Move Camera in direction
-            mainCamera.transform.Translate(zoomDir * zoomSpeed * Time.deltaTime * expectedFPS);
-        }
+        //if (dst > stopZoomingDist || zoomDir == -mainCamera.transform.forward)
+        //{
+        //    // Move Camera in direction
+        //    mainCamera.transform.Translate(zoomDir * zoomSpeed * Time.deltaTime * expectedFPS);
+        //}
 
-        previousDist = curDist;
+        //previousDist = curDist;
     }
     
-    private void Rotate(Vector3 rotateAround)
+    private void Rotate()
     {
-        // calculates the rotation based on which way the user moved 
-        rotDir = (Input.GetTouch(0).position - previousMousePos) * -1;
+        Vector3 curDist = Input.touches[0].position;
+        Vector3 prevPos = Input.touches[0].deltaPosition;
 
-        // Flip the rotation axis on x and y
-        rotDir.Set(rotDir.y, rotDir.x, rotDir.z);
-        rotDir = mainCamera.transform.InverseTransformDirection(rotDir);
+        if(curDist != prevPos)
+        {
+            Debug.Log("hg");
+            Vector3 rotDir = (curDist - prevPos);
+            cameraParent.transform.Rotate(rotDir.y, rotDir.x, 0, Space.Self);
+        }
+    }
+}
+public static class ExtensionMethods
+{
+    //public static Vector3 RotateAroundLocal(this Vector3 Point, Vector3 Direction, )
 
-        // rotates the camera about the desired point at an angle of rotation at rotation speed
-        mainCamera.transform.RotateAround(rotateAround, rotDir, rotSpeed * Time.deltaTime * expectedFPS);
-
-        // sets previous mouse position to first touch
-        previousMousePos = Input.GetTouch(0).position;
+    //Returns the rotated Vector3 using a Quaterion
+    public static Vector3 RotateAroundPivot(this Vector3 Point, Vector3 Pivot, Quaternion Angle)
+    {
+        return Angle * (Point - Pivot) + Pivot;
+    }
+    //Returns the rotated Vector3 using Euler
+    public static Vector3 RotateAroundPivot(this Vector3 Point, Vector3 Pivot, Vector3 Euler)
+    {
+        return RotateAroundPivot(Point, Pivot, Quaternion.Euler(Euler));
+    }
+    //Rotates the Transform's position using a Quaterion
+    public static void RotateAroundPivot(this Transform Me, Vector3 Pivot, Quaternion Angle)
+    {
+        Me.position = Me.position.RotateAroundPivot(Pivot, Angle);
+    }
+    //Rotates the Transform's position using Euler
+    public static void RotateAroundPivot(this Transform Me, Vector3 Pivot, Vector3 Euler)
+    {
+        Me.position = Me.position.RotateAroundPivot(Pivot, Quaternion.Euler(Euler));
     }
 }
